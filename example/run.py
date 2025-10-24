8c835b2c0f9f02eba300780957f283e2e5cd0268
Surxe <119145352+Surxe@users.noreply.github.com>
2025-10-23 21:19:01 -0500

    fixed example

--- example/run.py
+++ example/run.py
@@ -3,8 +3,9 @@ from options_schema import OPTIONS_SCHEMA
 import argparse
 
 arg_parser = argparse.ArgumentParser()
-arg_writer = ArgumentWriter()
+arg_writer = ArgumentWriter(schema=OPTIONS_SCHEMA)
 arg_writer.add_arguments(parser=arg_parser)
+args=arg_parser.parse_args()
 global logger
-options = init_options(args=arg_parser.parse_args(), schema=OPTIONS_SCHEMA, log_file="default.log")
+options = init_options(args=args, schema=OPTIONS_SCHEMA, log_file="default.log")
 from optionsconfig import logger
