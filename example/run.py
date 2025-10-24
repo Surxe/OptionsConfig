9bef1604b2a45a914d22e95851dbca2f81f58ce5
Surxe <119145352+Surxe@users.noreply.github.com>
2025-10-23 22:00:12 -0500

    fake args, expected log

--- example/run.py
+++ example/run.py
@@ -1,11 +1,11 @@
 from optionsconfig import init_options, ArgumentWriter
 from options_schema import OPTIONS_SCHEMA
 import argparse
-
+example_args = ['--enable-feature', '--feature-path', '/path/to/something']
 arg_parser = argparse.ArgumentParser()
 arg_writer = ArgumentWriter(schema=OPTIONS_SCHEMA)
 arg_writer.add_arguments(parser=arg_parser)
-args=arg_parser.parse_args()
+args=arg_parser.parse_args(example_args) # can use `python example/run.py --arg1 value1 --arg2 value2 etc.` then remove example_args here
 global logger
 options = init_options(args=args, schema=OPTIONS_SCHEMA, log_file="default.log")
 from optionsconfig import logger
